e3d25c039a62f9a97810bc1cc57827fe5cfbe055
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-06-17 14:36:06 +0300

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(userName, userEmail){
+    return fetch('http://localhost:3000/users', {
+        method:'POST',
+        headers:{
+            "Content-Type":"application/json",
+            "Accept":"application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+            
+
+        })
+
+    })
+    .then(response=>response.json())
+    .then(object=>{
+        const newId = object.id;
+        const p=document.createElement("p");
+        p.textContent = newId;
+        document.body.appendChild(p);
+        
+    })
+    .catch(error => {
+        const errorMessage = document.createElement("p");
+        errorMessage.textContent = error.message;
+        document.body.appendChild(errorMessage);
+    });
+}
